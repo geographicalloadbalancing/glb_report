7e0cb90990ad7715a3a8c0236b1c49eedccdc4d2
Michael Hirshleifer <111mth@caltech.edu>
2012-10-01 01:10:34 +0000

    add outline

---
+++ introduction_outline.mm
@@ -0,0 +1,31 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1349045627017" ID="ID_1163998920" MODIFIED="1349045815018" TEXT="Introduction">
+<node CREATED="1349046358422" ID="ID_674462964" MODIFIED="1349046374280" POSITION="right" TEXT="The problem">
+<node CREATED="1349045680174" ID="ID_39736586" MODIFIED="1349045924432" TEXT="Energy cost is important for DCs.">
+<node CREATED="1349046321115" ID="ID_1490688924" MODIFIED="1349046339991" TEXT="Servers use energy to process requests"/>
+<node CREATED="1349046174053" ID="ID_172428736" MODIFIED="1349046308862" TEXT="Energy usage for cooling also matters">
+<node CREATED="1349046719801" ID="ID_1597298838" MODIFIED="1349046726555" TEXT="Depends on weather and geography"/>
+</node>
+</node>
+<node CREATED="1349045949039" ID="ID_823596352" MODIFIED="1349045979917" TEXT="Load, energy availability, grid electricity prices, etc. are time-varying and stochastic.">
+<node CREATED="1349046030755" ID="ID_854975614" MODIFIED="1349046037413" TEXT="Renewable vs. grid energy"/>
+</node>
+<node CREATED="1349045982399" ID="ID_1143892650" MODIFIED="1349046004854" TEXT="Need for load balancing between DCs"/>
+</node>
+<node CREATED="1349046624788" ID="ID_145704538" MODIFIED="1349046630607" POSITION="right" TEXT="Previous work">
+<node CREATED="1349046630608" ID="ID_1721674031" MODIFIED="1349046644840" TEXT="Geographical load balancing">
+<node CREATED="1349046644841" ID="ID_1859669965" MODIFIED="1349046652037" TEXT="Can use almost entirely renewables"/>
+<node CREATED="1349046803984" ID="ID_70686434" MODIFIED="1349046944576" TEXT="Convex optimization framework; solve numerically"/>
+</node>
+</node>
+<node CREATED="1349046583417" ID="ID_708927763" MODIFIED="1349046588506" POSITION="right" TEXT="This paper">
+<node CREATED="1349046615296" ID="ID_884262881" MODIFIED="1349046898858" TEXT="Extend model to also consider cooling cost">
+<node CREATED="1349046745214" ID="ID_483433497" MODIFIED="1349046759799" TEXT="Exploit diversity in cooling costs across regions"/>
+<node CREATED="1349046815566" ID="ID_558891537" MODIFIED="1349046838252" TEXT="Reduces total cost and increases green energy usage"/>
+</node>
+<node CREATED="1349046870790" ID="ID_1249241819" MODIFIED="1349046876656" TEXT="Uses real data blah"/>
+<node CREATED="1349046686244" ID="ID_1581377221" MODIFIED="1349046689017" TEXT="Visualization"/>
+</node>
+</node>
+</map>
